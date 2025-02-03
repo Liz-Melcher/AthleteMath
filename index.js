c751804edc991c59b78b5886b208a2d9cce90761
Liz Melcher <lizmelchercodingbootcamp@gmail.com>
2025-02-03 13:15:00 -0600

    updated index files with examples

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 console.log("Index.js is loaded")
 
 
-function selectUnit(button) {
-    document.querySelectorAll('.btn-group .btn').forEach(btn => btn.classList.remove('active'));
-    button.classList.add('active');
-    }
